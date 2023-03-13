42c3b97ef04308b03c81030a85290c05e63971d8
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-03-13 18:13:23 +0100

    Add `Fir2IrConfiguration` class to store configuration parameters
    One example of such parameter is `linkViaSignatures`. There is
    `Psi2IrConfiguration` as an analog for K1.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/Fir2Ir.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/Fir2Ir.kt
@@ -19,6 +19,7 @@ import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.descriptors.deserialization.PlatformDependentTypeTransformer
 import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
 import org.jetbrains.kotlin.descriptors.konan.isNativeStdlib
+import org.jetbrains.kotlin.fir.backend.Fir2IrConfiguration
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporterFactory
 import org.jetbrains.kotlin.fir.backend.*
 import org.jetbrains.kotlin.fir.descriptors.FirModuleDescriptor
@@ -70,8 +71,8 @@ internal fun PhaseContext.fir2Ir(
 
     val (irModuleFragment, components, pluginContext, irActualizationResult) = input.firResult.convertToIrAndActualize(
             fir2IrExtensions,
+            Fir2IrConfiguration(linkViaSignatures = false),
             IrGenerationExtension.getInstances(config.project),
-            linkViaSignatures = false,
             signatureComposerCreator = null,
             irMangler = KonanManglerIr,
             firManglerCreator = { FirNativeKotlinMangler() },
